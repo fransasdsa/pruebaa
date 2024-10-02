485bb0e673c27ac64badc2af6d7690b96b93c8fb
fransasdsa <calizayaapazaf@gmail.com>
2024-10-02 13:33:41 -0500

    first commitgh

--- config-server/src/main/java/com/upu/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/com/upu/configserver/ConfigServerApplication.java
@@ -1,17 +1,15 @@
-package configserver.configserver;
+// src/main/java/com/upu/configserver/ConfigServerApplication.java
+package com.upu.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
-
 @SpringBootApplication
 @EnableConfigServer
-
 public class ConfigServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ConfigServerApplication.class, args);
     }
-
 }
--- config-server/src/test/java/com/upu/configserver/ConfigServerApplicationTests.java
+++ config-server/src/test/java/com/upu/configserver/ConfigServerApplicationTests.java
@@ -1,4 +1,4 @@
-package configserver.configserver;
+package com.upu.configserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
